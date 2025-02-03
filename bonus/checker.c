ae7dcf0c99dca6e1315247343dc7a00745ee538b
H0MZ0 <hamzared2500@gmail.com>
2025-02-03 16:28:47 +0100

    push

--- bonus/checker.c
+++ bonus/checker.c
@@ -6,32 +6,37 @@
 /*   By: hakader <hakader@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 16:08:51 by hakader           #+#    #+#             */
-/*   Updated: 2025/02/03 12:45:14 by hakader          ###   ########.fr       */
+/*   Updated: 2025/02/03 16:24:39 by hakader          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+#include <stdlib.h>
+
 char	*skip_spaces(char *str)
 {
-	char *dest;
-	int	i;
-	int	j;
+	int		i;
+	int		j;
 
-	dest = NULL;
+	if (!str)
+		return (NULL);
 	i = 0;
 	j = 0;
-	while(str[i])
+	while (str[i])
 	{
-		if (str[i] == 32 && str[i])
-			i++;
-		else
-			dest[j] = str[i];
+		if (str[i] != ' ') // Skip spaces and shift characters
+		{
+			str[j] = str[i];
+			j++;
+		}
 		i++;
-		j++;
 	}
-	return (dest);
+	str[j] = '\0'; // Null-terminate the modified string
+	return (str);
 }
+
+
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
@@ -42,6 +47,8 @@ int	ft_strcmp(const char *s1, const char *s2)
 	j = 0;
 	// while (s1[i] == 32)
 	// 	i++;
+	if (!s1 || !s2)
+		return (1);
 	while ((s1[i] || s2[j]))
 	{
 		if (s1[i] != s2[j])
@@ -54,29 +61,29 @@ int	ft_strcmp(const char *s1, const char *s2)
 
 void	get_checker(t_stack **stack_a, t_stack **stack_b, char *get)
 {
-	if (ft_strcmp(get, "sa\n") == 0)
+	if (strcmp(get, "sa\n") == 0)
 		sa(stack_a, 0);
-	else if (ft_strcmp(get, "sb\n") == 0)
+	else if (strcmp(get, "sb\n") == 0)
 		sb(stack_b, 0);
-	else if (ft_strcmp(get, "ss\n") == 0)
+	else if (strcmp(get, "ss\n") == 0)
 		ss(stack_a, stack_b, 0);
-	else if (ft_strcmp(get, "pa\n") == 0)
+	else if (strcmp(get, "pa\n") == 0)
 		pa(stack_a, stack_b, 0);
-	else if (ft_strcmp(get, "pb\n") == 0)
+	else if (strcmp(get, "pb\n") == 0)
 		pb(stack_a, stack_b, 0);
-	else if (ft_strcmp(get, "sb\n") == 0)
+	else if (strcmp(get, "sb\n") == 0)
 		sb(stack_b, 0);
-	else if (ft_strcmp(get, "ra\n") == 0)
+	else if (strcmp(get, "ra\n") == 0)
 		ra(stack_a, 0);
-	else if (ft_strcmp(get, "rb\n") == 0)
+	else if (strcmp(get, "rb\n") == 0)
 		rb(stack_b, 0);
-	else if (ft_strcmp(get, "rr\n") == 0)
+	else if (strcmp(get, "rr\n") == 0)
 		rr(stack_a, stack_b, 0);
-	else if (ft_strcmp(get, "rra\n") == 0)
+	else if (strcmp(get, "rra\n") == 0)
 		rra(stack_a, 0);
-	else if (ft_strcmp(get, "rrb\n") == 0)
+	else if (strcmp(get, "rrb\n") == 0)
 		rrb(stack_b, 0);
-	else if (ft_strcmp(get, "rrr\n") == 0)
+	else if (strcmp(get, "rrr\n") == 0)
 		(rrr(stack_a, stack_b, 0));
 	else
 	{
@@ -104,6 +111,7 @@ int	main(int ac, char **av)
 	size = stack_size(stack_a);
 	while ((get = get_next_line(0)))
 	{
+		printf("((%s))\n", get);
 		get = skip_spaces(get);
 		printf("{{%s}}\n", get);
 		get_checker (&stack_a, &stack_b, get);
